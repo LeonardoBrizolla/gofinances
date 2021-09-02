b72495c90264f7a41de280c206eb2544629a8faa
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-09-01 21:35:39 -0300

    Armazenando e recuperando dados with AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   Modal, 
   TouchableWithoutFeedback, 
@@ -7,6 +7,7 @@ import {
 } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { useForm } from 'react-hook-form';
 
@@ -50,6 +51,8 @@ export function Register() {
     key: "category",
     name: "Categoria"
   });
+  
+  const dataKey = '@gofinances:transactions'; 
 
   const {
     control,
@@ -71,7 +74,7 @@ export function Register() {
     setCategoryModalOpen(false);
   }
 
-  function handleRegister(form: FormData) {
+  async function handleRegister(form: FormData) {
     if (!transactionType) 
       return Alert.alert('Selecione o tipo da transação!');
       
@@ -85,8 +88,23 @@ export function Register() {
       category: category.key,
     }
     
-    console.log(data);
+    try {
+      await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível salvar");
+    }
   }
+
+useEffect(() => {
+  async function loadData() {
+    const data = await AsyncStorage.getItem(dataKey);
+    console.log(JSON.parse(data!));
+  }
+
+  loadData();
+}, []);
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
