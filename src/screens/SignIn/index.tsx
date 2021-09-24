e9c4fd05ae3c20c74abf419d863fe0a2ba49763d
Leonardo Brizolla <leonardobrizollars@gmail.com>
2021-09-24 15:30:37 -0300

    Conectando com uma conta Google

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Alert } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 import AppleSvg from '../../assets/apple.svg';
@@ -20,10 +21,18 @@ import {
 } from './styles';
 
 export function SignIn() {
-  const { user } = useAuth();
-
-  console.log(user.name);
+  const { signInWithGoogle } = useAuth();
   
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Nao foi possivel conectar na conta google! :(');
+    }
+  }
+
   return (
     <Container>
       <Header>
@@ -51,6 +60,7 @@ export function SignIn() {
           <SignInSocialButton 
             title="Entrar com Google"
             svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
           />
           <SignInSocialButton 
             title="Entrar com Apple"
